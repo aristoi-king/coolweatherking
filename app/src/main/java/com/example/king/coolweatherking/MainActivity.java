70830dc9e63cf64d050a7c25b8419684304765d9
Aristoi <aristoi@163.com>
2018-05-17 15:50:30 -0700

    First commit

---
+++ app/src/main/java/com/example/king/coolweatherking/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.king.coolweatherking;
+//register --hefengtianqi     coolweatherking123
+//API-Key 77c0a62274bc46889baecf8521fbe2dd
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
